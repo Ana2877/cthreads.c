69e970d8ad4ccbac26438456a2de6e897e59db94
Augusto Bardini <azbardini@gmail.com>
2019-10-06 17:15:07 -0300

    :white_check_mark: Updated semaphore test

--- testes/csemaphore_test.c
+++ testes/csemaphore_test.c
@@ -9,26 +9,43 @@ csem_t mutex;
 void *return_void_star()
 {
     cwait(&mutex);
-    DEBUG("SLEEP");
-    sleep(3);
-    DEBUG("SLEPT");
+    printf("WAITEI\n");
+    int x = 0;
+    while (x<1000000000) {
+      x++;
+    }
+    x = 0;
+    cyield();
+    while (x<1000000000) {
+      x++;
+    }
+    x = 0;
+    printf("SOLTEI O CU PRA RUUUDA TUDO\n");
     csignal(&mutex);
     return NULL;
 }
 
 int return_int()
 {
-    cwait(&mutex);
-    DEBUG("TENTA ENTRAR");
-    csignal(&mutex);
-    return 0;
+  int x = 0;
+  printf("VO TENTA RUUDA TUDO\n");
+  cwait(&mutex);
+  while (x<1000000000) {
+    x++;
+  }
+  printf("SÓ PRINTO DEPOIS CPX\n");
+  csignal(&mutex);
+  return 0;
 }
 
 int main()
 {
     csem_init(&mutex, 1);
-    ccreate(&return_void_star, NULL, 0);
-    ccreate((void *)&return_int, NULL, 0);
+    int primeira = ccreate(&return_void_star, NULL, 0);
+    int segunda = ccreate((void *)&return_int, NULL, 0);
 
+    printf("Waiting for primeira e segunda (%d %d)\n", primeira, segunda);
+    cjoin(primeira);
+    cjoin(segunda);
     return 0;
 }
